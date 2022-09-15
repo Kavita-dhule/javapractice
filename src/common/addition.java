1c916dfb64b4a44145bf2010f147f02ef57ad886
kavita-dhule <kavita.rahurkar11@gmail.com>
2022-09-15 17:50:22 +0530

    first commit

---
+++ src/common/addition.java
@@ -0,0 +1,17 @@
+package common;
+
+public class addition {
+   public static void main(String[] args) {
+        int a=10;
+        int b=30;
+      float c;
+              float f=9.80f;
+
+              c =a * f;
+         System.out.println(c);
+
+
+
+    }
+
+}
